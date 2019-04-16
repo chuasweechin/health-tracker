0e4d6a9783edfd68bfbfe2af9466c4010d2c049b
chuasweechin <sampiochua@gmail.com>
2019-04-16 10:38:06 +0800

    update database schema

--- sql/tables.sql
+++ sql/tables.sql
@@ -1,35 +1,39 @@
 CREATE TABLE IF NOT EXISTS user (
     username TEXT PRIMARY KEY,
     password TEXT,
-    created_at TEXT
+    created_at TIMESTAMP
 );
 
 CREATE TABLE IF NOT EXISTS stat (
     id SERIAL PRIMARY KEY,
     username TEXT,
+    birth_date TIMESTAMP,
     bmi INTEGER,
     bmi_category TEXT,
     weight_in_kg INTEGER,
     weight_in_pound INTEGER,
     height_in_cm INTEGER,
     weight_in_m INTEGER,
-    created_at TEXT
+    created_at TIMESTAMP
 );
 
 CREATE TABLE IF NOT EXISTS activity (
     id SERIAL PRIMARY KEY,
     name TEXT,
     type TEXT,
-    metabolic_equivalent INTEGER,
-    compendium_code TEXT
+    compendium_code TEXT,
+    kcal_per_kg_hour INTEGER,
+    kcal_per_kg_minute INTEGER,
+    kcal_per_kg_second INTEGER
 );
 
 CREATE TABLE IF NOT EXISTS activity_log (
     id SERIAL PRIMARY KEY,
     username TEXT,
-    activity_name TEXT,
+    activity_id TEXT,
     count INTEGER,
     duration INTEGER,
     kilocalories_burned INTEGER,
-    created_at TEXT
+    created_at TIMESTAMP,
+    updated_at TIMESTAMP
 );
